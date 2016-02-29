3b37e3e568500d1cd7399c77027efd03f554620c
himynameisjeremy <himynameisjeremy@gmail.com>
2016-02-29 00:01:58 -0600

    fixed bugs

--- deathstar.js
+++ deathstar.js
@@ -11,33 +11,38 @@ $(document).ready(function(){
     kirKanosArray.push(hk47);
     monlthlyCreditsSpent();
   });
-  monlthlyCreditsSpent();
+  //monlthlyCreditsSpent();
 });
 
 var kirKanosArray = [];
+var monthlyCreds = 0;
 
 function monlthlyCreditsSpent(){
-  var monthlyCreds = 0;
+
   for(var i = 0; i < kirKanosArray.length; i++){
     var kyleKatarn = kirKanosArray[i];
-    monthlyCreds += parseInt(kyleKatarn.yearlySal/12);
+    //monthlyCreds += parseInt(kyleKatarn.yearlySal/12);
     $('.tieFighter').text("Name: " + kyleKatarn.empName);
     $('.tieBomber').text("ID Number: " + kyleKatarn.empIDnum);
     $('.tieInterceptor').text("Job Title: " + kyleKatarn.jobTitle);
     $('.tieAdvanced').text("Yearly Salary: " + kyleKatarn.yearlySal);
     var employeeCount = i + 1;
-    $('.ebonHawk').text("Total monthly cost of salaries: $" + monthlyCreds);
-    $('.c3PO').text("Total employees counted: " + employeeCount);
+    //$('.ebonHawk').text("Total monthly cost of salaries: $" + monthlyCreds);
+    //$('.c3PO').text("Total employees counted: " + employeeCount);
     //This is where I append the data
-    $('.rakataPrime').append('<div></div>');
-    var carnorJax = $('.rakataPrime').children().last();
-    carnorJax.append('<p>' + hk47.empName + ' ' + hk47.empIDnum + ' ' + hk47.jobTitle + ' ' + hk47.yearlySal);
-    carnorJax.append('<p>Name: ' + hk47.empName + '</p>');
-    carnorJax.append('<p>' + hk47.empIDnum + '</p>');
-    carnorJax.append('<p>' + hk47.jobTitle + '</p>');
-    carnorJax.append('<p>' + hk47.yearlySal + '</p>');
+    //$('.rakataPrime').append('<div></div>');
+    //var carnorJax = $('.rakataPrime').children().last();
+    //carnorJax.append('<p>#' + employeeCount + ') NAME: ' + hk47.empName + ' ID NUMBER: ' + hk47.empIDnum + ' JOB TITLE: ' + hk47.jobTitle + ' YEARLY SALARY: ' + hk47.yearlySal);
   }
+  monthlyCreds += parseInt(kyleKatarn.yearlySal/12);
+  //var employeeCount = i + 1;
+  $('.ebonHawk').text("Total monthly cost of salaries: $" + monthlyCreds);
+  $('.c3PO').text("Total employees counted: " + employeeCount);
+
+  $('.rakataPrime').append('<div></div>');
+  var carnorJax = $('.rakataPrime').children().last();
+  carnorJax.append('<p>#' + employeeCount + ') NAME: ' + hk47.empName + ' ID NUMBER: ' + hk47.empIDnum + ' JOB TITLE: ' + hk47.jobTitle + ' YEARLY SALARY: ' + hk47.yearlySal);
   console.log(kirKanosArray);
   console.log(monthlyCreds);
-
+  console.log(hk47);
 }
