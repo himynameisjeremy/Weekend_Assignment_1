beaf41763bb1db33a16d200e0809777b127378b1
himynameisjeremy <himynameisjeremy@gmail.com>
2016-02-28 23:15:16 -0600

    saving data

--- deathstar.js
+++ deathstar.js
@@ -1,7 +1,9 @@
+var hk47 = {};
+
 $(document).ready(function(){
   $('#darthRevan').on('submit', function(event){
     event.preventDefault();
-    var hk47 = {};
+
     $.each($('#darthRevan').serializeArray(), function(i,field){
       hk47[field.name] = field.value;
     });
@@ -26,6 +28,14 @@ function monlthlyCreditsSpent(){
     var employeeCount = i + 1;
     $('.ebonHawk').text("Total monthly cost of salaries: $" + monthlyCreds);
     $('.c3PO').text("Total employees counted: " + employeeCount);
+    //This is where I append the data
+    $('.rakataPrime').append('<div></div>');
+    var carnorJax = $('.rakataPrime').children().last();
+    carnorJax.append('<p>' + hk47.empName + ' ' + hk47.empIDnum + ' ' + hk47.jobTitle + ' ' + hk47.yearlySal);
+    carnorJax.append('<p>Name: ' + hk47.empName + '</p>');
+    carnorJax.append('<p>' + hk47.empIDnum + '</p>');
+    carnorJax.append('<p>' + hk47.jobTitle + '</p>');
+    carnorJax.append('<p>' + hk47.yearlySal + '</p>');
   }
   console.log(kirKanosArray);
   console.log(monthlyCreds);
